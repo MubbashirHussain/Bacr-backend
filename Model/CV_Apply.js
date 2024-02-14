f752fbf6105d888662bd5d329f045c45e00d846d
MubbashirHussain <mubbashirhhussain@gmail.com>
2024-02-15 00:05:26 +0500

    .

--- Model/CV_Apply.js
+++ Model/CV_Apply.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 let Schema = new mongoose.Schema({
-        Team : {type :String ,  enum: ["ManagemntTeam", "HVACRTeam" , "ServiceTeam"] , required : true},
+        Team : {type :String ,  enum: ["ManagementTeam", "HVACRTeam" , "ServiceTeam"] , required : true},
         ApplyingFor : {type :String  , required : true},
         FullName : {type :String , required : true},
         CV_Url : {type :String , required : true},
